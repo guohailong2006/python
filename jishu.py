f213106517e2576b0c39bad545132b985935ab09
root <you@example.com>
2017-12-06 07:43:27 -0500

    add first commit

---
+++ jishu.py
@@ -0,0 +1,6 @@
+start=1
+while True:
+  if start==51:
+     break
+  print start
+  start +=2
---
+++ mysql.py
@@ -0,0 +1,3 @@
+import MySQLdb
+conn=MySQLdb.connect('127.0.0.1','root','Gcheng314159','info')
+print conn 
